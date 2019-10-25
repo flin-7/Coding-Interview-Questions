443376e2915191c666db02cc382c180fdb95b787
flin-7 <flin.7@icloud.com>
2019-10-24 23:33:33 -0700

    FizzBuzz

---
+++ src/com/felix/FizzBuzz.java
@@ -0,0 +1,17 @@
+package com.felix;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        fizzBuzz(15);
+    }
+
+    public static void fizzBuzz(int x) {
+        for (int i = 1; i <= x; i++) {
+            if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
+            else if (i % 3 == 0) System.out.println("Fizz");
+            else if (i % 5 == 0) System.out.println("Buzz");
+            else System.out.println(i);
+        }
+    }
+}
