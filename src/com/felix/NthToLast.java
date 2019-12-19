ce88ab407ccee8422b3952d98e4e32b5327f42fb
flin-7 <flin.7@icloud.com>
2019-12-18 16:59:26 -0800

    12/18/19

---
+++ src/com/felix/NthToLast.java
@@ -0,0 +1,28 @@
+package com.felix;
+
+public class NthToLast {
+
+    private class Node {
+        private int value;
+        private Node next;
+    }
+
+    public Node nthToLast(Node node, int n) {
+        Node curr = node;
+        Node follower = node;
+
+        for (int i = 0; i < n; i++) {
+            if (curr == null) {
+                return null;
+            }
+            curr = curr.next;
+        }
+
+        while (curr.next!= null) {
+            curr = curr.next;
+            follower = follower.next;
+        }
+
+        return follower;
+    }
+}
